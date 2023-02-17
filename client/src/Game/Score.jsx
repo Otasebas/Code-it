09a24206ba456bed9d45d07520367260c31d1a7f
+ <+>
2023-02-17 11:28:56 -0500

    the game works

--- client/src/Game/Score.jsx
+++ client/src/Game/Score.jsx
@@ -1,25 +1,36 @@
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 function Score({user}){
     
     const navigate = useNavigate()
 
+    const[scores, setScores] = useState([])
+
     function handleClick(id){
         navigate(`/game/${id}`)
     }
 
-    return(
+    useEffect(()=>{
+        fetch(`/scores/`)
+        .then(res=>res.json())
+        .then(req=> setScores(req))
+    },[user.id])
+
+    return (
         <div>
-            {user.scores.map(score =>{
+            {scores.map(score =>{
                 return(
                     <div key={score.id}>
-                        <div>{score.score} : {score.deck.name}</div>
-                        <button onClick={()=> handleClick(score.deck.id)} > Play Again? </button>
+                        <div>{score.score} : {score.deck_name}</div>
+                        <button onClick={()=> handleClick(score.deck_id)} > Play Again? </button>
                     </div>
                 )
             })}
         </div>
     )
 }
+    
 
 export default Score
