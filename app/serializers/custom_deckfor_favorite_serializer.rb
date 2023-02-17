ec8d9d93e38cdeb525a62e146c703169dfaa53ca
Tucker-Judge <116079954+Tucker-Judge@users.noreply.github.com>
2023-02-16 18:28:15 -0600

    Revert "css"

--- app/serializers/custom_deckfor_favorite_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CustomDeckforFavoriteSerializer < ActiveModel::Serializer
-  attributes :id, :cards
-
-  has_many :cards
-end
--- client/src/Home.jsx/Header.jsx
+++ client/src/Home.jsx/Header.jsx
@@ -29,16 +29,11 @@ function Header({user, setUser}){
     }
     
     return(
-
-        <div className = "navbar">
-            <img className = "nav-logo" alt = "empty array" src="https://blog.soliditylang.org/img/avatar-icon.png" onClick={(navigate("/"))}/>
-            <button  > Public </button>
-
+        <div>
             <button  onClick={handlePublic}> Public Decks </button>
-
             <button  > Following </button>
-            <button onClick={handleFavorites} > Favorites </button>
             <button onClick={handleDecks} > Create Decks </button>
+            <button onClick={handleFavorites} > Favorites </button>
             <button onClick={handleScores} > Personal Scores </button>
             <button onClick={handleLogOut} > Log Out </button>
         </div>
